08d684e163837ca919bf1999ed4d1dbf3c4a1ac2
Saramberr12 <higginssarah12@gmail.com>
2018-12-10 15:52:29 -0500

    add all digits that appear next to even sums:

--- ProblemTwo.java
+++ ProblemTwo.java
@@ -1,10 +1,11 @@
 public class ProblemTwo {
 
-    static int a = 1; 
-    static int b = 2;
-    static int c = 1;
+    static int a = 0; 
+    static int b = 1;
+    static int c = 0;
 
     static void fib(int count) {
+        int sum = 0;
         if (count > 0) {
             c = a + b;
             a = b;
@@ -12,15 +13,24 @@ public class ProblemTwo {
 
             System.out.print(" " + c);
 
+            if (c % 2 == 0) {
+                sum += c;
+            }
+
             //to make recursive, include: 
             fib(count - 1);
+
+            System.out.println();
+            System.out.print("Even sums: " + sum);
         }
     }
 
     public static void main(String args[]) {
-        int count = 10;
+        int count = 35;
+        int sum = 0;
+
         System.out.print("Fibonacci sequence is: ");
         System.out.print(a + " " + b);
-        fib(count - 2);  
+        fib(count - 2);
     }
 }
